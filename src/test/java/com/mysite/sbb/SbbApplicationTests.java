7aadbf955ecfdeb742701ee66da028b105e3f199
jhs512 <jangka512@gmail.com>
2022-04-30 16:33:33 +0900

    3-02 페이징, 대량 테스트 데이터 만들기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,10 +20,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class SbbApplicationTests {
 
     @Autowired
+    private QuestionService questionService;
+
+    @Autowired
     private QuestionRepository questionRepository;
 
     @Autowired
     private AnswerRepository answerRepository;
+
+    @Test
+    void testMakeQuestionMany() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = "테스트 데이터입니다 : [%03d]".formatted(i);
+            String content = "내용무 : %d".formatted((int) (Math.random() * 100));
+            questionService.create(subject, content);
+        }
+    }
 
     @Test
     @Transactional
