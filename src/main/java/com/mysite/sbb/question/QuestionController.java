2e57abd2b4a63723fce416ace1af1988cf95db35
jhs512 <jangka512@gmail.com>
2022-05-08 16:54:36 +0900

    3-13 ë§4 검색

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -27,9 +27,10 @@ public class QuestionController {
     private UserService userService;
 
     @RequestMapping("/list")
-    public String list(Model model, @RequestParam(value = "page", defaultValue = "1") int page) {
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "1") int page,  @RequestParam(value = "kw", defaultValue = "") String kw) {
         page--;
-        Page<Question> paging = questionService.getList(page);
+        Page<Question> paging = questionService.getList(page, kw);
+        model.addAttribute("kw", kw);
         model.addAttribute("paging", paging);
 
         return "question_list";
